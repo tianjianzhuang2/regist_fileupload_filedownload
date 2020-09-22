c661d272342186de6911e3cb1a2d385fb4851c0d
tianjianzhuang2 <tjz19930515@PC-20121206>
2020-09-22 22:06:13 +0800

    Student类

--- Java20200922/src/com/tjz/test/MyTest.java
+++ Java20200922/src/com/tjz/test/MyTest.java
@@ -4,6 +4,9 @@ public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("hahhah");
 		System.out.println("aaa");
+		
+		Student s = new Student();
+		System.out.println(s);
 	}
 
 }
